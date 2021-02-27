67297e68f951c8d59cdc05bcc8d904e0b699223a
Ezequiel Teixeira Cruz <Ezequiel@NOTE>
2021-02-26 22:08:31 -0300

    Usando Toast

--- Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/MainActivity.java
+++ Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/MainActivity.java
@@ -1,14 +1,14 @@
 package br.com.treinamento.radix.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Ezequiel Teixeira Cruz", Toast.LENGTH_LONG).show();
     }
 }
