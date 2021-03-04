724b9f51eff15a85d81c9452b71517000722d945
Ezequiel T. Cruz <ezequielteixeiracr@gmail.com>
2021-03-04 19:47:24 -0300

    Implementando layout de lista

--- Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/MainActivity.java
+++ Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/MainActivity.java
@@ -2,6 +2,11 @@ package br.com.treinamento.radix.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Ezequiel", "Julio", "Arthur")
+        );
+
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
