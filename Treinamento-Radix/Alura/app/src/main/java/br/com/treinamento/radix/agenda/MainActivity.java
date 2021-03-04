a96f650933c6220a66a3bf3370ab97ffed544b9c
Ezequiel T. Cruz <ezequielteixeiracr@gmail.com>
2021-03-04 19:22:03 -0300

    Utilizando textView

--- Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/MainActivity.java
+++ Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.treinamento.radix.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Ezequiel Teixeira Cruz", Toast.LENGTH_LONG).show();
+        TextView aluno = new TextView(this);
+        aluno.setText("Ezequiel T. Cruz");
+        setContentView(aluno);
     }
 }
