9a6ed018e60b7b4fdf178b662ec7972ab7c4d8a7
Ezequiel T. Cruz <ezequielteixeiracr@gmail.com>
2021-03-04 19:34:45 -0300

    utilizando xml

--- Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/MainActivity.java
+++ Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/MainActivity.java
@@ -2,16 +2,12 @@ package br.com.treinamento.radix.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
-        aluno.setText("Ezequiel T. Cruz");
-        setContentView(aluno);
+        setContentView(R.layout.activity_main);
     }
 }
