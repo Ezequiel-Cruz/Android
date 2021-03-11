c2404f0ea0940bbe20a7d014b5115ca709637abc
Ezequiel T. Cruz <ezequielteixeiracr@gmail.com>
2021-03-11 19:52:23 -0300

    criação do FormularioAlunoActivity

---
+++ Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package br.com.treinamento.radix.agenda.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.treinamento.radix.agenda.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
