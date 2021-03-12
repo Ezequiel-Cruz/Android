c44a1bd1a017efa2893826d58aa147a115da7c48
Ezequiel T. Cruz <ezequielteixeiracr@gmail.com>
2021-03-12 00:09:19 -0300

    tela de formulário

--- Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/ui/activity/ListaAlunosActivity.java
+++ Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,4 +1,4 @@
-package br.com.treinamento.radix.agenda;
+package br.com.treinamento.radix.agenda.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,19 +9,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.treinamento.radix.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de Alunos");
 
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Ezequiel", "Julio", "Arthur", "Ana", "Marlene")
         );
 
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
