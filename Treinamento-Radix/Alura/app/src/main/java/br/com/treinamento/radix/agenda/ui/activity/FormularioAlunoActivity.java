d13c95f117215ca4627158a3b296fd68af774e16
Ezequiel T. Cruz <ezequielteixeiracr@gmail.com>
2021-03-12 13:07:02 -0300

    Criação do modelo

--- Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/ui/activity/FormularioAlunoActivity.java
+++ Treinamento-Radix/Alura/app/src/main/java/br/com/treinamento/radix/agenda/ui/activity/FormularioAlunoActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import br.com.treinamento.radix.agenda.R;
+import br.com.treinamento.radix.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
 
@@ -15,13 +17,24 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+        final EditText campoNome = findViewById(R.id.activity_formulario_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_email);
 
         Button botaoSalvar = findViewById(R.id.activity_formulario_botao_salvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Aluno alunoCriado = new Aluno(nome, telefone, email);
+
                 Toast.makeText(FormularioAlunoActivity.this,
-                        "Cliquei no botão salvar",
+                        alunoCriado.getNome() + " - "
+                            + alunoCriado.getTelefone() + " - "
+                            + alunoCriado.getEmail(),
                         Toast.LENGTH_SHORT).show();
             }
         });
