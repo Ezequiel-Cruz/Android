1b041fa5d3cad444f3e219bacb0d0d642a635bb9
Ezequiel T. Cruz <ezequielteixeiracr@gmail.com>
2021-03-15 11:55:10 -0300

    Utilizando setOnItemClickListener

--- Agenda/app/src/main/java/br/com/treinamento/radix/agenda/ui/activity/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/br/com/treinamento/radix/agenda/ui/activity/ListaAlunosActivity.java
@@ -2,7 +2,9 @@ package br.com.treinamento.radix.agenda.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,6 +14,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import br.com.treinamento.radix.agenda.R;
 import br.com.treinamento.radix.agenda.dao.AlunoDAO;
+import br.com.treinamento.radix.agenda.model.Aluno;
 
 public class ListaAlunosActivity extends AppCompatActivity {
     public static final String TITULO_APPBAR = "Lista de Alunos";
@@ -24,6 +27,9 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         setTitle(TITULO_APPBAR);
         configuraFabNovoAluno();
+        dao.salva(new Aluno("Ezequiel","999999999", "adm@adm.com"));
+        dao.salva(new Aluno("Julio","999999999", "adm@adm.com"));
+        dao.salva(new Aluno("Julia","999999999", "adm@adm.com"));
     }
 
     private void configuraFabNovoAluno() {
@@ -52,5 +58,12 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 this,
                 android.R.layout.simple_list_item_1,
                 dao.todos()));
+
+        listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int posicao, long id) {
+                Log.i("posicaoAluno" , "" + posicao);
+            }
+        });
     }
 }
